e92342d8b75700580467769f6982b155158361e6
joaovitorper <jovipepa.10@gmail.com>
2025-06-25 12:03:14 -0300

    criar funções e funciona charmander e pikachu

---
+++ script.js
@@ -0,0 +1,9 @@
+function ativarpikachu(){
+document.getElementById ("pokemon-img").src="imagens/pikachu.jpg";
+}
+function ativarcharmander(){
+document.getElementById("pokemon-img").src="imagens/charmander.png";
+}
+function ativarsquirtle(){
+
+}
